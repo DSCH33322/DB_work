5f31769a5bc69984d984900e671cef6cc2edc3bb
DSCH33322 <DS.git@web.de>
2022-02-14 13:27:17 +0000

    WHERE als Filter Delete - DS löschen | Vorsicht!!

--- 04_SQL_Dataimport/04b_db_csv_import.sql
+++ 04_SQL_Dataimport/04b_db_csv_import.sql
@@ -9,7 +9,23 @@ UPDATE boo.cats SET cat_name = "Alonzo" WHERE id = 2;
 UPDATE boo.cats SET fur_color = "grey" WHERE id = 3;
 
 /* READ - SELECT : NEUE Ergebnistabelle wird erstellt */
-SELECT * FROM boo.cats; -- gesamte Tabelle
-SELECT cat_name FROM boo.cats; -- einzelnes Feld
-SELECT cat_name, age FROM boo.cats; -- kombinierte Felder 
-SELECT cat_name AS "Katzen", age AS "Alter" FROM boo.cats; -- Felder / neue Namen
+#SELECT * FROM boo.cats; -- gesamte Tabelle
+#SELECT cat_name FROM boo.cats; -- einzelnes Feld
+#SELECT cat_name, age FROM boo.cats; -- kombinierte Felder 
+#SELECT cat_name AS "Katzen", age AS "Alter" FROM boo.cats; -- Felder / neue Namen
+
+/* Filtern durch WHERE*/
+SELECT 
+    cat_name AS "Katzen",
+    age AS "Alter"
+FROM boo.cats 
+#WHERE id = 1 
+#WHERE fur_color = "grey"
+#WHERE fur_color = "alpinaweiss" -- empty fail
+WHERE age >= 30
+;
+
+/* Delete - DS löschen | Vorsicht!! */
+#DELETE FROM boo.cats; -- Alle Daten sind weg!!
+#DELETE FROM boo.cats WHERE id = 2; -- Filter
+
