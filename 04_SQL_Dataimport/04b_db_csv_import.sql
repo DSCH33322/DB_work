1175e348e5a06f1354c52d2e75d6d76e48faffe0
DSCH33322 <DS.git@web.de>
2022-02-14 11:53:46 +0000

    UPDATE Alonzo

--- 04_SQL_Dataimport/04b_db_csv_import.sql
+++ 04_SQL_Dataimport/04b_db_csv_import.sql
@@ -3,3 +3,8 @@
 -- CREATE siehe 04_ ...ALTER
 
 SELECT * FROM boo.cats;
+
+/* UPDATE --> Änderung(en) in bestehender Tab. */
+UPDATE boo.cats SET cat_name = "Alonzo" WHERE id = 2;
+
+SELECT * FROM boo.cats;
