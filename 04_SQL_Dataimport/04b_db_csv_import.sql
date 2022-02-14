aec281ab8a85c849447b5a3199a64dbb0fa2fca0
DSCH33322 <DS.git@web.de>
2022-02-14 11:54:56 +0000

    UPDATE grey

--- 04_SQL_Dataimport/04b_db_csv_import.sql
+++ 04_SQL_Dataimport/04b_db_csv_import.sql
@@ -6,5 +6,6 @@ SELECT * FROM boo.cats;
 
 /* UPDATE --> Änderung(en) in bestehender Tab. */
 UPDATE boo.cats SET cat_name = "Alonzo" WHERE id = 2;
+UPDATE boo.cats SET fur_color = "grey" WHERE id = 3;
 
 SELECT * FROM boo.cats;
