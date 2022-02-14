d8341fc901096feb9457110a7647b1270ee230f6
DSCH33322 <DS.git@web.de>
2022-02-14 13:10:55 +0000

    READ - SELECT

--- 04_SQL_Dataimport/04b_db_csv_import.sql
+++ 04_SQL_Dataimport/04b_db_csv_import.sql
@@ -8,4 +8,8 @@ SELECT * FROM boo.cats;
 UPDATE boo.cats SET cat_name = "Alonzo" WHERE id = 2;
 UPDATE boo.cats SET fur_color = "grey" WHERE id = 3;
 
-SELECT * FROM boo.cats;
+/* READ - SELECT : NEUE Ergebnistabelle wird erstellt */
+SELECT * FROM boo.cats; -- gesamte Tabelle
+SELECT cat_name FROM boo.cats; -- einzelnes Feld
+SELECT cat_name, age FROM boo.cats; -- kombinierte Felder 
+SELECT cat_name AS "Katzen", age AS "Alter" FROM boo.cats; -- Felder / neue Namen
