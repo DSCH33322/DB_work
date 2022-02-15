b2771ef3680509a5cca45941f75b62c76ebbc83d
DSCH33322 <DS.git@web.de>
2022-02-15 08:16:20 +0000

    05_SQL_SELECT erstellt data erstellt und CSV-Dateien eingefügt

---
+++ 05_SQL_SELECT/05_2_que_basic.sql
@@ -0,0 +1,15 @@
+/* QUERIES Basic */
+
+/*
+Felder abfragen
+Felder kombinieren 
+Ausgabe beschränken
+*/
+
+-- Abfrage aller Spalten oder Tab
+#SELECT * FROM stocks.ccc;
+
+-- Begrenzung: LIMIT
+-- Abfrage eines Feldes oder einer Feld-Kombi m. Umbenennung (Alias)
+-- Kombination aus mehreren Feldern in neues Feld 
+-- hier mit Verknüpfung CONCAT() // String-Fkt. 
