6e41cccb3d24ceca89b08a57028b5a347c4ae34b
DSCH33322 <DS.git@web.de>
2022-02-15 09:05:00 +0000

    LIMIT

--- 05_SQL_SELECT/05_2_que_basic.sql
+++ 05_SQL_SELECT/05_2_que_basic.sql
@@ -13,3 +13,14 @@ Ausgabe beschränken
 -- Abfrage eines Feldes oder einer Feld-Kombi m. Umbenennung (Alias)
 -- Kombination aus mehreren Feldern in neues Feld 
 -- hier mit Verknüpfung CONCAT() // String-Fkt. 
+SELECT
+    ticker AS "SYM",
+	price AS "Kurs in $",
+    c_name AS "Unternehmen",
+	#industry AS "Branche"
+	CONCAT(sector, "|", industry) AS "Operations"
+FROM stocks.ccc
+LIMIT 10 -- X Zeilen ab 0
+#LIMIT 200,10
+;
+
