e4116d4d633391ee4e4711c5fca5371199a60db4
DSCH33322 <DS.git@web.de>
2022-02-15 09:42:23 +0000

    ORDER BY no_yrs DESC,sector ASC aktiv

--- 05_SQL_SELECT/05_3_que_sort.sql
+++ 05_SQL_SELECT/05_3_que_sort.sql
@@ -8,21 +8,21 @@ SELECT
 	price AS "Kurs in $",
     no_yrs AS " Jahre ++",   
 	#industry AS "Branche"
-	CONCAT(sector, "|", industry) AS "Operations"
+	CONCAT(sector, " | ", industry) AS "Operations"
 FROM stocks.ccc
 -- Sortierung 
 #ORDER BY ticker ASC -- SYM, alphabetisch, aufsteigend
 #ORDER BY ticker DESC -- SYM, alphabetisch, absteigend 
 
-#ORDER BY price ASC -- Kurs, beginnend mit der günstigsten Aktie 
-#ORDER BY price DESC -- Kurs, beginnend mit der teuersten Aktie 
-#ORDER BY no_yrs DESC -- Aktien mit der längsten Div.-Hostorie
+#ORDER BY price ASC -- Kurs, numerisch beginnend mit der günstigsten Aktie 
+#ORDER BY price DESC -- Kurs, numerisch beginnend mit der teuersten Aktie 
+#ORDER BY no_yrs DESC -- numerisch, Aktien mit der längsten Div.-Hostorie
 
 #ORDER BY sector DESC,industry ASC --  Kombination ASC / DESC alphabetisch
-#ORDER BY no_yrs DESC,sector ASC -- Kombination ASC / DESC alphabetisch / numerisch 
+ORDER BY no_yrs DESC,sector ASC -- Kombination ASC / DESC alphabetisch / numerisch 
 
 
 -- Begrenzung
-LIMIT 40 -- X Zeilen ab 0
+LIMIT 60 -- X Zeilen ab 0
 #LIMIT 200,10
 ;
