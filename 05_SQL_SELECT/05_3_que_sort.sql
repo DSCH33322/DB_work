378c6cb8408d3f2e0715a9e7254249d13e3b556f
DSCH33322 <DS.git@web.de>
2022-02-15 09:25:53 +0000

    ASK / DESK

--- 05_SQL_SELECT/05_3_que_sort.sql
+++ 05_SQL_SELECT/05_3_que_sort.sql
@@ -1,2 +1,28 @@
 /* QUERIES Sortierung */
 
+-- ASC / DESC
+
+SELECT
+    ticker AS "SYM",
+    c_name AS "Unternehmen",
+	price AS "Kurs in $",
+    no_yrs AS " Jahre ++",   
+	#industry AS "Branche"
+	CONCAT(sector, "|", industry) AS "Operations"
+FROM stocks.ccc
+-- Sortierung 
+#ORDER BY ticker ASC -- SYM, alphabetisch, aufsteigend
+#ORDER BY ticker DESC -- SYM, alphabetisch, absteigend 
+
+#ORDER BY price ASC -- Kurs, beginnend mit der günstigsten Aktie 
+#ORDER BY price DESC -- Kurs, beginnend mit der teuersten Aktie 
+#ORDER BY no_yrs DESC -- Aktien mit der längsten Div.-Hostorie
+
+#ORDER BY sector DESC,industry ASC --  Kombination ASC / DESC alphabetisch
+#ORDER BY no_yrs DESC,sector ASC -- Kombination ASC / DESC alphabetisch / numerisch 
+
+
+-- Begrenzung
+LIMIT 40 -- X Zeilen ab 0
+#LIMIT 200,10
+;
